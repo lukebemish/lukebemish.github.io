58d8ea3ae2b96a576e7239e71cee0e7bc2b9dbf4
Luke Bemish <lukebemish@lukebemish.dev>
2025-11-20 23:26:17 -0500

    A Year in Pencils 2025
    Also set up automatic webp generation and fix style

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,14 +16,6 @@
             wordCountTarget: '.post-word-count',
         });
         
-        // Creates Captions from Alt tags
-        $(".post-content img").each(function() {
-            // Let's put a caption if there is one
-            if($(this).attr("alt") && !$(this).hasClass("emoji"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
-        });
-        
     });
 
 }(jQuery));
